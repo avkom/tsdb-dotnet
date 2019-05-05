7f658872e6cc6c0e46a46c785e9d3502ee22113f
Oleksandr Komendant <avkomq@gmail.com>
2019-05-05 01:14:17 -0400

    Add _labelsByHash Dictionary to Index class

--- src/TSDB/Index.cs
+++ src/TSDB/Index.cs
@@ -1,20 +1,25 @@
 ﻿using System.Collections.Generic;
-
 namespace TSDB
 {
     public class Index : IIndex
     {
-        private readonly Dictionary<string, Dictionary<string, SortedSet<ulong>>> _labels = 
+        private readonly Dictionary<ulong, List<Label>> _labelsByHash = new Dictionary<ulong, List<Label>>();
+
+        //                         LabelName         LabelValue          Hash
+        //                             |                  |                |
+        private readonly Dictionary<string, Dictionary<string, SortedSet<ulong>>> _hashesByLabel = 
             new Dictionary<string, Dictionary<string, SortedSet<ulong>>>();
 
         public void Insert(ulong hash, List<Label> labels)
         {
+            _labelsByHash.Add(hash, labels);
+
             foreach (Label labelToInsert in labels)
             {
-                if (!_labels.TryGetValue(labelToInsert.Name, out var label))
+                if (!_hashesByLabel.TryGetValue(labelToInsert.Name, out var label))
                 {
                     label = new Dictionary<string, SortedSet<ulong>>();
-                    _labels.Add(labelToInsert.Name, label);
+                    _hashesByLabel.Add(labelToInsert.Name, label);
                 }
 
                 if (!label.TryGetValue(labelToInsert.Value, out var hashes))
