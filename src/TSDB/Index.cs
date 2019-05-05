015a58624f6d6163f691ef6b78a30e2476f6854c
Oleksandr Komendant <avkomq@gmail.com>
2019-05-04 23:34:28 -0400

    Implement Index.Insert() method

--- src/TSDB/Index.cs
+++ src/TSDB/Index.cs
@@ -4,8 +4,27 @@ namespace TSDB
 {
     public class Index : IIndex
     {
+        private readonly Dictionary<string, Dictionary<string, SortedSet<ulong>>> _labels = 
+            new Dictionary<string, Dictionary<string, SortedSet<ulong>>>();
+
         public void Insert(ulong hash, List<Label> labels)
         {
+            foreach (Label labelToInsert in labels)
+            {
+                if (!_labels.TryGetValue(labelToInsert.Name, out var label))
+                {
+                    label = new Dictionary<string, SortedSet<ulong>>();
+                    _labels.Add(labelToInsert.Name, label);
+                }
+
+                if (!label.TryGetValue(labelToInsert.Value, out var hashes))
+                {
+                    hashes = new SortedSet<ulong>();
+                    label.Add(labelToInsert.Value, hashes);
+                }
+
+                hashes.Add(hash);
+            }
         }
 
         public List<TimeSeriesMetadata> Query(List<LabelMatcher> matchers)
