4588f8ce6226cc57dfc99852b49e689660805ef1
Oleksandr Komendant <avkomq@gmail.com>
2019-05-04 23:41:26 -0400

    Implement SampleStorage class

--- src/TSDB/SampleStorage.cs
+++ src/TSDB/SampleStorage.cs
@@ -1,21 +1,33 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace TSDB
 {
     public class SampleStorage : ISampleStorage
     {
+        private readonly Dictionary<ulong, List<Sample>> _series = new Dictionary<ulong, List<Sample>>();
+
         public bool Exists(ulong hash)
         {
-            return false;
+            return _series.ContainsKey(hash);
         }
 
         public List<Sample> Get(long startTimestamp, long endTimestamp, ulong hash)
         {
-            return new List<Sample>();
+            return _series[hash]
+                .Where(sample => sample.Timestamp >= startTimestamp && sample.Timestamp <= endTimestamp)
+                .ToList();
         }
 
         public void Insert(ulong hash, List<Sample> samples)
         {
+            if (!_series.TryGetValue(hash, out var series))
+            {
+                series = new List<Sample>();
+                _series.Add(hash, series);
+            }
+
+            series.AddRange(samples);
         }
     }
 }
